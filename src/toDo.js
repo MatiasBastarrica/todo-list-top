a98e6c1e8af99b3577b0e065b411c5007e4c10b7
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-02 06:24:15 -0300

    Add a method to rename a toDo

--- src/toDo.js
+++ src/toDo.js
@@ -1,9 +1,13 @@
-class ToDo {
+export class ToDo {
   constructor(title, description, dueDate, priority, status) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.status = status;
+  }
+
+  rename(category, value) {
+    this.category = value;
   }
 }
