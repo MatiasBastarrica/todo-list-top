16086d32f40ff50db008caba9b3a8d9f272021eb
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-10 20:57:51 -0300

    Start setting the localStorage

--- src/index.js
+++ src/index.js
@@ -11,21 +11,46 @@ const project1 = new Project(
   "This is just an experimental project",
 );
 
-// project1.addToDo(
-//   "Reading",
-//   "Continue reading 'Brothers Karamazov'",
-//   "tonight",
-//   2,
-//   "unfinished",
-// );
+project1.addToDo(
+  "Reading",
+  "Continue reading 'Brothers Karamazov'",
+  "tonight",
+  "high",
+  false,
+);
 
-// project1.addToDo(
-//   "Hygiene",
-//   "Take a shower after cleaning",
-//   "Now",
-//   5,
-//   "unfinished",
-// );
+project1.addToDo(
+  "Hygiene",
+  "Take a shower after cleaning",
+  "Now",
+  "medium",
+  false,
+);
+
+const project2 = new Project(
+  "My second project",
+  "This is just an experimental project",
+);
+
+project2.addToDo(
+  "Writing",
+  "Continue writing your journal",
+  "next week",
+  "urgent",
+  false,
+);
+
+project2.addToDo(
+  "Listening",
+  "Listen the new episode of the Grit",
+  "tomorrow",
+  "low",
+  false,
+);
+
+const projects = [project1, project2];
+
+localStorage.setItem("projects", JSON.stringify(projects));
 
 // console.log(project1);
 
